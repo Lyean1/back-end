b7cf6f02e1506061f2f35e808b4dbaf0e82f8c15
Lyean <lyean1500@gmail.com>
2023-06-06 18:34:01 +0630

    initial commit

---
+++ dist/users/users.routes.config.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.UsersRoutes = void 0;
+const common_routes_config_1 = require("../common/common.routes.config");
+class UsersRoutes extends common_routes_config_1.CommonRoutesConfig {
+    constructor(app) {
+        super(app, 'UsersRoutes');
+    }
+    configureRoutes() {
+        return this.app;
+    }
+}
+exports.UsersRoutes = UsersRoutes;
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoidXNlcnMucm91dGVzLmNvbmZpZy5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIi4uLy4uL3VzZXJzL3VzZXJzLnJvdXRlcy5jb25maWcudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7O0FBQUEseUVBQW9FO0FBSXBFLE1BQWEsV0FBWSxTQUFRLHlDQUFrQjtJQUMvQyxZQUFZLEdBQXdCO1FBQ2hDLEtBQUssQ0FBQyxHQUFHLEVBQUUsYUFBYSxDQUFDLENBQUM7SUFDOUIsQ0FBQztJQUVELGVBQWU7UUFDWCxPQUFPLElBQUksQ0FBQyxHQUFHLENBQUM7SUFDcEIsQ0FBQztDQUdKO0FBVkQsa0NBVUMifQ==
